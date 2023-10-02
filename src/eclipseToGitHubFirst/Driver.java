8c731e42e3b984bc313d547897e8b7258189e5db
mevett <mevett@emich.edu>
2023-10-02 16:53:01 -0400

    Main workspace altered 2nd line

--- src/eclipseToGitHubFirst/Driver.java
+++ src/eclipseToGitHubFirst/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("This this!!");
-		System.out.println("added another line!");
+		System.out.println("added another line (changed by main workspace)!");
 
 	}
 
